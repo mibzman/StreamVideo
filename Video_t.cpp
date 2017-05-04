f1d22587d41aaaef8954f9a2530e71c22c3b69ab
Michael Swartzentruber <Michael Swartzentruber>
2017-05-04 18:15:45 -0400

    the first test has been added

--- Video_t.cpp
+++ Video_t.cpp
@@ -5,12 +5,17 @@
 */
 
 #include <cassert>
+#include <iostream>
+#include <string>
 
 #include "Video.hpp"
 
 int main() {
 
 	// TODO: Add test cases
-	
+	//test case 1
+	//test the basic constructor
+		Video StarWars (std::string episode1 = "A new Hope", int a=5, int timeH= 2, int timeM= 35, int epi=0 );
+
 	return 0;
 }
