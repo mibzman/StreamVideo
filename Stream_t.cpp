9fe097efa953f38d912ff669460c0ef1b1ab557f
Sam Borick <mibzman@27thisotope.ddns.net>
2017-05-08 17:54:06 -0400

    added getter and setter test

--- Stream_t.cpp
+++ Stream_t.cpp
@@ -8,9 +8,34 @@
 
 #include "Stream.hpp"
 
+bool videosAreEqual(Video video1, Video video2){
+	return
+		video1.getTitle() == video1.getTitle() ||
+		video1.getType() == video1.getType() ||
+		video1.getHours() == video1.getHours() ||
+		video1.getMinutes() == video1.getMinutes() ||
+		video1.getEpisodes() == video1.getEpisodes();
+}
+
 int main() {
 
-    // TODO: Add test cases
+	//test getOccurrences
+    {
+    	Video video("The Lord of the Rings: The Fellowship of the Ring", 0, 3, 18, 0);
+    	Stream stream(video, 1);
+
+    	assert(stream.getOccurrences() == 1);
+    }
+
+    	//test getVideo
+    {
+    	Video video("The Lord of the Rings: The Fellowship of the Ring", 0, 3, 18, 0);
+    	Stream stream(video, 1);
+
+    	Video outputVideo = stream.getVideo();
+    	assert(videosAreEqual(video, outputVideo));
+    }
+    
 
     return 0;
 }
