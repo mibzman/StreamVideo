dce2fc6468eefb5d40799846c85354f335b13f09
Michael Swartzentruber <Michael Swartzentruber>
2017-05-08 16:47:48 -0400

    formatting changed

--- Video_t.cpp
+++ Video_t.cpp
@@ -15,32 +15,32 @@ int main() {
 	//test case 1
 	//test the basic constructor
 	{
-		Video StarWars ("A new Hope",5,2,35,0 );
+    Video StarWars ("A new Hope",5,2,35,0 );
 	}
 	{
-		Video StarWars ("A new Hope",5,2,35,0 );
-		//now test the getTitle
-		assert(StarWars.getTitle()=="A new Hope");
+    Video StarWars ("A new Hope",5,2,35,0 );
+    //now test the getTitle
+    assert(StarWars.getTitle()=="A new Hope");
   }
 	{
-		Video StarWars ("A new Hope",5,2,35,0 );
-		//now test the getTitle
-		assert(StarWars.getType()==5);
+    Video StarWars ("A new Hope",5,2,35,0 );
+    //now test the getTitle
+    assert(StarWars.getType()==5);
   }
   {
-		Video StarWars ("A new Hope",5,2,35,0 );
-		//now test the getTitle
-		assert(StarWars.getHours()==2);
+    Video StarWars ("A new Hope",5,2,35,0 );
+    //now test the getTitle
+    assert(StarWars.getHours()==2);
   }
   {
-		Video StarWars ("A new Hope",5,2,35,0 );
-		//now test the getTitle
-		assert(StarWars.getMinutes()==35);
+    Video StarWars ("A new Hope",5,2,35,0 );
+    //now test the getTitle
+    assert(StarWars.getMinutes()==35);
   }
   {
-		Video StarWars ("A new Hope",5,2,35,0 );
-		//now test the getTitle
-		assert(StarWars.getEpisodes()==0);
+    Video StarWars ("A new Hope",5,2,35,0 );
+    //now test the getTitle
+    assert(StarWars.getEpisodes()==0);
   }
-	return 0;
+  return 0;
 }
