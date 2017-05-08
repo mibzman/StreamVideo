949cbb702ce2a9443f6a6e1b4688a2ed236878f3
Michael Swartzentruber <Michael Swartzentruber>
2017-05-08 16:50:49 -0400

    formatting changed

--- Video_t.cpp
+++ Video_t.cpp
@@ -10,19 +10,18 @@
 #include "Video.hpp"
 
 int main() {
-
-	// TODO: Add test cases
-	//test case 1
-	//test the basic constructor
-	{
+  // TODO: Add test cases
+  //test case 1
+  //test the basic constructor
+  {
     Video StarWars ("A new Hope",5,2,35,0 );
-	}
-	{
+  }
+  {
     Video StarWars ("A new Hope",5,2,35,0 );
     //now test the getTitle
     assert(StarWars.getTitle()=="A new Hope");
   }
-	{
+  {
     Video StarWars ("A new Hope",5,2,35,0 );
     //now test the getTitle
     assert(StarWars.getType()==5);
